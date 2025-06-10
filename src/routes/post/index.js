c962f9414eed4e49c421884050c13bda0bda87ec
lebestnoob <lebestnoob12@gmail.com>
2025-06-10 13:25:30 -0700

    just passthrough all the data for the post

--- src/routes/post/index.js
+++ src/routes/post/index.js
@@ -22,24 +22,7 @@ app.get("/:id", async(c) => {
                     is_video = true;
                 }
 
-                post_meta = [{
-                    taken_at: items.taken_at,
-                    like_and_view_counts_disabled: items.like_and_view_counts_disabled,
-                    shortcode: items.code,
-                    caption_is_edited: items.caption_is_edited,
-                    id: items.id,
-                    carousel_media_count: items.carousel_media_count,
-                    user: {
-                        id: items.user.pk,
-                        username: items.user.username,
-                        full_name: items.user.full_name,
-                        is_private: items.user.is_private,
-                        is_verified: items.user.is_verified,
-                    },
-                    like_count: items.like_count,
-                    caption: items.caption.text,
-                    comment_count: items.comment_count,
-                }];
+                post_meta = items;
             });
     } catch (err) {
         console.warn("Invalid post or rate limited!");
